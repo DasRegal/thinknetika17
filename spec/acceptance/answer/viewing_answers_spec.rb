cc5b354b05ae210de79d815db8c6e67fc03e4910
Vladimir Elizarov <vladimirelizarov89@gmail.com>
2017-08-13 00:31:35 +0300

    add viewing answers spec

---
+++ spec/acceptance/answer/viewing_answers_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature 'viewing answers', %q{
+  All users can view list of answers to question
+} do 
+  given!(:question) { create(:question_with_answers) }
+  given(:user){ create(:user) }
+
+  after do 
+    expect(page).to have_content(question.title)
+    expect(page).to have_content(question.body)
+    question.answers.each do |answer|
+      expect(page).to have_content(answer.body)
+    end
+    save_and_open_page
+  end
+
+  scenario 'Non-authenticate user tries to view list of questions' do 
+    visit question_path(question)
+  end
+
+  scenario 'Authenticate user tries to view list of questions' do 
+    sign_in(user)
+    visit question_path(question)
+  end
+end
