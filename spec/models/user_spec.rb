385e9743f40fe6b1a55f52efd3b5aa4d72bcb299
Vladimir Elizarov <vladimir.elizarov@megafon.ru>
2017-08-14 13:51:39 +0300

    add test for author_of?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe User do
   it { should have_many(:answers).dependent(:destroy) }
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+  it 'author_of methods work correct' do 
+    user = create :user
+    question = create :question
+    answer = create :answer
+    expect(user.author_of?(question)).to eq false
+    expect(user.author_of?(answer)).to eq false
+
+    question.user = user
+    answer.user = user
+    expect(user.author_of?(question)).to eq true
+    expect(user.author_of?(answer)).to eq true
+  end
 end
