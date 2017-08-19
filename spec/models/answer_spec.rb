7b42de7e30eed3080f08a1618bb2cda71f4d7a20
Vladimir Elizarov <vladimirelizarov89@gmail.com>
2017-08-19 04:08:18 +0300

    fix answer model spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,38 +4,22 @@ RSpec.describe Answer, type: :model do
   it { should belong_to :question }
   it { should belong_to :user }
   it { should validate_presence_of :body }
-  # context 'setting answer to be best' do 
-  #   before do 
-  #     @question = create :question_with_answers
-  #   end
+  context 'setting answer to be best' do 
+    before do 
+      @question = create :question_with_answers
+    end
 
-  #   it 'have only 1 best answer' do 
-  #     @question.answers.each do |a|
-  #       @question.set_best_answer(a)
-  #     end
+    it 'have only 1 best answer' do 
+      @question.answers.each do |a|
+        a.set_best
+      end
+      expect(@question.answers.bests.count).to eq 1
+    end
 
-  #     expect(@question.answers.bests.count).to eq 1
-  #   end
-
-  #   it 'set best answer' do 
-  #     answer = @question.answers.first
-  #     @question.set_best_answer(answer)
-  #     expect(answer.best_answer?).to eq true
-  #   end
-
-  #   it 'set best only if answer is child of question' do 
-  #     answer = create(:answer)
-  #     @question.set_best_answer(answer)
-  #     expect(@question.answers.bests.count).to eq 0
-  #     expect(answer.best_answer?).to eq false
-  #   end
-
-  #   it 'dont change best answer if not child' do 
-  #     answer = create(:answer)
-  #     @question.set_best_answer(@question.answers.first)
-  #     old_best_answer = @question.answers.best
-  #     @question.set_best_answer(answer)
-  #     expect(@question.answers.best).to eq old_best_answer
-  #   end
-  # end
+    it 'set best answer' do 
+      answer = @question.answers.first
+      answer.set_best
+      expect(answer.is_best?).to eq true
+    end
+  end
 end
