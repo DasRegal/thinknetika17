0b57313224362eec02be511c09a13637c5bbf522
Vladimir Elizarov <vladimirelizarov89@gmail.com>
2017-08-20 09:47:32 +0300

    attachment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817095910) do
+ActiveRecord::Schema.define(version: 20170820064409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20170817095910) do
     t.bigint "user_id"
     t.boolean "is_best", default: false
     t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "attachments", force: :cascade do |t|
+    t.string "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "question_id"
+    t.index ["question_id"], name: "index_attachments_on_question_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -52,5 +60,6 @@ ActiveRecord::Schema.define(version: 20170817095910) do
   end
 
   add_foreign_key "answers", "users"
+  add_foreign_key "attachments", "questions"
   add_foreign_key "questions", "users"
 end
