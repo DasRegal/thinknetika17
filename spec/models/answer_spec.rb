af3c3c5408fb1795808b295dfee73e23c7bb7f1e
Vladimir Elizarov <vladimirelizarov89@gmail.com>
2017-08-20 10:33:09 +0300

    polymorph attachments

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it { should belong_to :question }
   it { should belong_to :user }
+  it { should have_many(:attachments).dependent(:destroy) }
   it { should validate_presence_of :body }
+
+  it { should accept_nested_attributes_for :attachments }
+  
   context 'setting answer to be best' do 
     before do 
       @question = create :question_with_answers
