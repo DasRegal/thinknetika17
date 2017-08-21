0ffa401ae530b175d8b348e688c2870997a31696
Vladimir Elizarov <vladimirelizarov89@gmail.com>
2017-08-21 23:03:56 +0300

    now can add multyply files to answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,7 +4,7 @@ class Answer < ApplicationRecord
   has_many :attachments, as: :attachmentable, dependent: :destroy
   validates  :body, presence: true
 
-  accepts_nested_attributes_for :attachments
+  accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
 
   scope :bests, -> { where(is_best: true) }
   
