b0115e6f1dcdc210b6b0298eb6432dfcb65b285f
Vladimir Elizarov <vladimirelizarov89@gmail.com>
2017-08-22 00:05:40 +0300

    add attachments controller

--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -2,4 +2,8 @@ FactoryGirl.define do
   factory :attachment do
     file "MyString"
   end
+
+  factory :question_attach, class: "Attachment" do 
+    association :attachmentable, factory: :attachment
+  end
 end
