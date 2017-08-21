94d98c70c75988858a7a0d860503d83e0af73171
Vladimir Elizarov <vladimirelizarov89@gmail.com>
2017-08-21 22:43:14 +0300

    add multyple files to question

--- spec/acceptance/question/add_files_to_question_spec.rb
+++ spec/acceptance/question/add_files_to_question_spec.rb
@@ -19,4 +19,18 @@ feature 'Add files to question', %q{
     click_on 'Create'
     expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
   end
+
+  scenario 'User can add multiply files when asks question', js: true do 
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'Test body'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'add file'
+    within all('.nested-fields').last do 
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    end
+    click_on 'Create'
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/2/spec_helper.rb'
+    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/3/rails_helper.rb'
+    expect(page).to have_content 'Your question successfully created.'
+  end
 end
