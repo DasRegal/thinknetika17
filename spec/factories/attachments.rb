d716f1f4642276e074774a7691c19ccdd45674c5
Vladimir Elizarov <vladimir.elizarov@megafon.ru>
2017-08-22 10:43:58 +0300

    add delete attach from answer spec

--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -6,6 +6,10 @@ FactoryGirl.define do
   factory :question_attach, class: "Attachment" do 
     association :attachmentable, factory: :attachment
     file { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'spec_helper.rb')) }
+  end
 
+  factory :answer_attach, class: "Attachment" do 
+    association :attachmentable, factory: :attachment
+    file { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'spec_helper.rb')) }
   end
 end
