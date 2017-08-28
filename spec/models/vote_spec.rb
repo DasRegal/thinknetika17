2ab386e8e07caf4ea651c173941f4b63e1166619
Vladimir Elizarov <vladimir.elizarov@megafon.ru>
2017-08-28 15:29:23 +0300

    add vote model spec

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -4,4 +4,38 @@ RSpec.describe Vote, type: :model do
   it { should belong_to :user }
   it { should belong_to :voteable }
   it { should validate_presence_of :status }
+
+  context '.vote?' do 
+    before do 
+      @user = create(:user)
+      @question = create(:question)
+      vote_up = create(:vote_up, user: @user, voteable: @question)
+      @question2 = create(:question)
+    end
+
+    it 'return true if user has vote in this obj' do 
+      expect(@question.vote?(@user, 'up')).to eq true
+    end
+
+    it 'return false if user dont has vote in this obj ' do
+      expect(@question2.vote?(@user, 'up')).to eq false 
+    end
+  end
+
+  context '.vote' do 
+    before do 
+      @user = create(:user)
+      @question = create(:question)
+      @vote_up = create(:vote_up, user: @user, voteable: @question)
+      @question2 = create(:question)
+      @question.vote(@user, 'down')
+    end
+
+    it 'destroy all votes on this obj on this user' do 
+    end
+
+    it 'create new vote' do 
+      
+    end
+  end
 end
