baaaad8bdd8c482bb02b42f3f8321ddeaa094a48
Vladimir Elizarov <vladimir.elizarov@megafon.ru>
2017-09-04 17:12:54 +0300

    add test for creating answers with ws

---
+++ spec/acceptance/comment/create_comment_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../acceptance_helper'
+
+feature 'Comment for answer and question', %q{
+  In order to commening answers and questions
+  As an authenticate user
+  I want to be able to commenting from question page
+} do 
+
+end
